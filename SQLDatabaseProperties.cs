3ddf7525aea3adcc8a9586025910e61b38b68f65
nhutluong <Nhut.Luong@mscoding.org>
2022-07-15 13:33:37 -0500

    added sql database properties class

---
+++ SQLDatabaseProperties.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql_contactslist
+{
+    internal class SQLDatabaseProperties
+    {
+        public int id { get; set; } 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; } 
+        public string nickNamwe { get; set; }
+        public string email { get; set; }   
+        public string address { get; set; }
+        public string birthday { get; set; }    
+        public string notes { get; set; }
+        public string company { get; set; }
+        public Boolean favorites { get; set; }
+        public Boolean active { get; set; }
+
+        public string resultData
+        {
+            get { return firstName + " " + lastName; }
+        }
+
+    }
+}
